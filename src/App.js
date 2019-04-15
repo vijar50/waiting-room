ed7b764a538e5dd3a943106f7fc9aa32178466d0
vijar50 <40033437+vijar50@users.noreply.github.com>
2019-04-15 12:12:27 +0100

    setup material ui theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { blue, blueGrey } from "@material-ui/core/colors";
+import Typography from "@material-ui/core/Typography";
+
+//Global theme colors
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: blue[400]
+    },
+    primary: {
+      main: blueGrey[700]
+    }
+  }
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>Hello World</header>
+        <MuiThemeProvider theme={theme}>
+          {/* React Routing will go here */}
+          <Typography color="primary" variant="h1">
+            Hello World
+          </Typography>
+        </MuiThemeProvider>
       </div>
     );
   }
