ccb034939b153fb629323462866a24fb242ff3c1
Mahmood <M26967@LE00412101.euro.ngci.com>
2019-04-24 12:28:12 +0100

    fixed

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class Registration extends Component {
   render() {
-    return <div>Registration Form changeMe</div>;
+    return <div>Registration Form changeMe!!</div>;
   }
 }
