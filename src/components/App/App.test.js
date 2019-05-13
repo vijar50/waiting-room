b2fa14b0c499d0a46f69934dd911f17b774f2b9f
vijar50 <40033437+vijar50@users.noreply.github.com>
2019-05-13 09:13:04 +0100

    Add Unit Tests for Routes

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { shallow } from "enzyme";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 import App from "./App";
 
-describe("App", () => {
+//RENDERING
+describe("<App/> - rendering", () => {
   let wrapper;
   beforeEach(() => (wrapper = shallow(<App />)));
 
   it("should render correctly", () => expect(wrapper).toMatchSnapshot());
+
+  it("should contain a <MuiThemeProvider/>", () => {
+    expect(wrapper.find(MuiThemeProvider).length).toEqual(1);
+  });
 });
+//INTERACTIONS
+
+//LIFECYCLE METHOD INVOCATIONS
