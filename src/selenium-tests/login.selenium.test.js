09b6457defc5ac9eeef199eaba58ab0202ac7b63
Malkani <M24004@uk.emea.ngint.com>
2019-06-24 14:28:44 +0100

    #92 First two login tests

--- src/selenium-tests/login.selenium.test.js
+++ src/selenium-tests/login.selenium.test.js
@@ -5,26 +5,57 @@ const assert = require("assert");
 const { Builder, Key, By, until } = require("selenium-webdriver");
 jest.setTimeout(30000);
 
-describe("Checkout Google.com", function() {
+describe("Login Tests", function() {
   let driver;
-  beforeAll(async function() {
+  beforeEach(async function() {
     driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("http://localhost:3000/#/");
+    await driver
+      .findElement(By.className("MuiGrid-item-15 MuiGrid-grid-xs-6-48"))
+      .click();
+    await driver.sleep(2000);
   });
-  // Next, we will write steps for our test.
-  // For the element ID, you can find it by open the browser inspect feature.
-  it("Search on Google", async function() {
-    // Load the page
-    await driver.get("https://google.com");
-    // Find the search box by id
-    await driver.findElement(By.name("q")).click();
-    // Enter keywords and click enter
-    await driver.findElement(By.name("q")).sendKeys("bubbles", Key.RETURN);
-    // Wait for the results box by id
-    await driver.wait(until.elementLocated(By.id("rcnt")), 10000);
-    // We will get the title value and test it
-    let title = await driver.getTitle();
-    assert.equal(title, "bubbles - Google Search");
+
+  it("should log in successfully", async () => {
+    await driver.wait(
+      until.elementLocated(By.xpath("//input[@id='username']")),
+      10000
+    );
+    await driver
+      .findElement(By.xpath("//input[@id='username']"))
+      .sendKeys("bubbles", Key.RETURN);
+    await driver
+      .findElement(By.xpath("//input[@id='password']"))
+      .sendKeys("bubbles", Key.RETURN);
+    await driver.wait(
+      until.elementLocated(
+        By.className(
+          "MuiButtonBase-root-200 MuiButton-root-174 MuiButton-contained-185 MuiButton-containedSecondary-187 MuiButton-raised-188 MuiButton-raisedSecondary-190 Login-button-7"
+        )
+      ),
+      10000
+    );
+    await driver
+      .findElement(
+        By.className(
+          "MuiButtonBase-root-200 MuiButton-root-174 MuiButton-contained-185 MuiButton-containedSecondary-187 MuiButton-raised-188 MuiButton-raisedSecondary-190 Login-button-7"
+        )
+      )
+      .click();
+    await driver.wait(until.elementLocated(By.id("foo")));
+    await driver.sleep(3000);
+  });
+
+  it("should navigate back from cancel", async () => {
+    await driver
+      .findElement(By.xpath("//span[contains(text(),'Cancel')]"))
+      .click();
+    await driver.sleep(2000);
+    await driver.wait(
+      until.elementLocated(By.className("MuiGrid-item-15 MuiGrid-grid-xs-6-48"))
+    );
   });
   // close the browser after running tests
-  afterAll(() => driver && driver.quit());
+  afterEach(() => driver && driver.quit());
 });
+
